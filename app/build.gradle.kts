fcdcc597905e255ce0924cfc98917fbaeff7d084
Antonin Musil <tonik.musil@email.cz>
2024-09-29 17:10:48 +0200

    V1.1.0 - update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 10
-        versionName = "1.0.5"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
