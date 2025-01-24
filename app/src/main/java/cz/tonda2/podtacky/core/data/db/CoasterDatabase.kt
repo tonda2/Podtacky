9b1ddbdc13bad4db6c1a163405fac658e0bb8a88
Antonin Musil <tonik.musil@email.cz>
2025-01-24 11:23:23 +0100

    V1.2.0 - add folder table, table column to coaster, migrate db

--- app/src/main/java/cz/tonda2/podtacky/core/data/db/CoasterDatabase.kt
+++ app/src/main/java/cz/tonda2/podtacky/core/data/db/CoasterDatabase.kt
@@ -4,17 +4,42 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import cz.tonda2.podtacky.features.coaster.data.db.CoasterDao
 import cz.tonda2.podtacky.features.coaster.data.db.DbCoaster
+import cz.tonda2.podtacky.features.coaster.data.db.DbFolder
 
-@Database(version = 1, entities = [DbCoaster::class])
+@Database(
+    version = 2,
+    entities = [DbCoaster::class, DbFolder::class]
+)
 abstract class CoasterDatabase : RoomDatabase() {
 
     abstract fun coasterDao(): CoasterDao
 
     companion object {
         fun newInstance(context: Context): CoasterDatabase {
-            return Room.databaseBuilder(context, CoasterDatabase::class.java, "coaster.db").build()
+            return Room.databaseBuilder(context, CoasterDatabase::class.java, "coaster.db")
+                .addMigrations(MIGRATION_1_2)
+                .build()
         }
+    }
+}
+
+private val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("""
+            CREATE TABLE IF NOT EXISTS folders (
+                folderId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                name TEXT NOT NULL,
+                parentId INTEGER,
+                uploaded INTEGER NOT NULL DEFAULT 0,
+                deleted INTEGER NOT NULL DEFAULT 0,
+                FOREIGN KEY(parentId) REFERENCES folders(folderId) ON DELETE NO ACTION
+            )
+        """.trimIndent())
+
+        db.execSQL("ALTER TABLE coasters ADD COLUMN folderId INTEGER")
     }
 }
---
+++ app/src/main/java/cz/tonda2/podtacky/features/coaster/data/db/DbFolder.kt
@@ -0,0 +1,23 @@
+package cz.tonda2.podtacky.features.coaster.data.db
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "folders",
+    foreignKeys = [
+        ForeignKey(
+            entity = DbFolder::class,
+            parentColumns = arrayOf("folderId"),
+            childColumns = arrayOf("parentId")
+        )
+    ]
+)
+data class DbFolder(
+    @PrimaryKey(autoGenerate = true) val folderId: Long = 0,
+    val name: String = "",
+    val parentId: Long? = null,
+    val uploaded: Boolean = false,
+    val deleted: Boolean = false
+)
