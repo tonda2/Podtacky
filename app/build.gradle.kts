5834fc3a82d47aec91370a3bc378bc6ec7d16905
Antonin Musil <tonik.musil@email.cz>
2025-02-01 13:25:30 +0100

    v1.1.2 - don't use flow for single coaster to speed up navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "cz.tonda2.podtacky"
         minSdk = 24
         targetSdk = 34
-        versionCode = 16
+        versionCode = 17
         versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
